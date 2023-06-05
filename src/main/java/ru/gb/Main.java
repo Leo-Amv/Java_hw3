802649591f0d9d1abd0e8e4aca9f610ae01b971e
Leo <leonidamvrosov@yandex.ru>
2023-06-05 22:49:22 +0500

    workers array added

--- src/main/java/ru/gb/Main.java
+++ src/main/java/ru/gb/Main.java
@@ -2,6 +2,12 @@ package ru.gb;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BaseWorker [] workers = {
+                new HourlyWorker(15),
+                new FixedWorker(3000)
+        };
+        for (BaseWorker worker: workers) {
+            System.out.println(worker.avgMonthlySalary());
+        }
     }
 }
